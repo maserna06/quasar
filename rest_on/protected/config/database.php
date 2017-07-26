e74dfd9e6d41b0ab0ccdadad47f74bd4c6381312
miguelparadisosolutions <miguel.p@paradisosolutions.com>
2017-07-25 22:59:12 -0500

    restablecer database file

---
+++ rest_on/protected/config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host=localhost;dbname=rests_on',
+	'emulatePrepare' => true,
+	//'username' => 'rests_on',
+	//'password' => 'kiY5a72$',
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8',
+	'enableParamLogging' => true,
+	
+);
